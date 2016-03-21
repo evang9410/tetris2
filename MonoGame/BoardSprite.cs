cb7302867ee955310bcc21b2898e42ef7622723e
trexboners <evang9410@gmail.com>
2016-03-21 13:22:07 -0400

    started 3

---
+++ MonoGame/BoardSprite.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Tetris;
+
+namespace MonoGame
+{
+    class BoardSprite: Game1
+    {
+        private IBoard board;
+        private Game1 game;
+        private SpriteBatch spriteBatch;
+
+        //To render
+        Texture2D emptyBlock;
+        Texture2D filledBlock;
+
+        //Constructor
+        public BoardSprite(Game1 game, IBoard board)
+        {
+            this.game = game;
+            this.board = board;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
+    }
+}
